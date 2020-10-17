7f47bebc902333071e76df4f5f013fa515d7a753
Christopher Warren <chrisalmith@gmail.com>
2020-10-17 17:53:58 -0400

    proxy fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    ["/api/*", "/auth/google", "/upload"],
+    ["/api/*", "/auth/*", "/upload"],
     createProxyMiddleware({
       target: "http://localhost:5000",
     })
